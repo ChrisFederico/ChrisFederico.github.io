91911333f250c100b6f06d68e8f6a48083d52d7b
Cristiano Federico <cristiano.federico.92@gmail.com>
2021-03-21 16:29:52 +0100

    From dynamic to static

--- resources/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="mt-auto text-white-50">
-    <p>Developed by <a href="https://github.com/ChrisFederico" class="text-white">Chris Federico</a></p>
-</footer>
--- resources/header.php
+++
@@ -1,9 +0,0 @@
-<header class="mb-auto">
-    <div>
-        <h3 class="float-md-start mb-0"></h3>
-        <nav class="nav nav-masthead justify-content-center float-md-end">
-            <a class="nav-link active text-white" aria-current="page" href="index.php">Home</a>
-            <a class="nav-link text-white" href="writeups.php">Write-Ups</a>
-        </nav>
-    </div>
-</header>
