6401d3cc6db172ebbca24b0d8cbf723bce24856d
Cristiano Federico <cristiano.federico.92@gmail.com>
2021-03-21 16:06:42 +0100

    Revert "test"
    This reverts commit 54cb737f6bee57efba66ce8b9e4156ca9ddb8005.

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
-<!-- <?php $writeups = require $_SERVER['DOCUMENT_ROOT'] . '/data/writeups_data.php'; ?> -->
+<?php $writeups = require $_SERVER['DOCUMENT_ROOT'] . '/data/writeups_data.php'; ?>
 
 <!doctype html>
 <html lang="en" class="h-100">
   
-    <!-- <?php require "resources/head.php"; ?> -->
+    <?php require "resources/head.php"; ?>
 
 	<body class="d-flex h-100 text-center text-white bg-dark">
     
 		<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-			<!-- <?php require "resources/header.php"; ?>
+			<?php require "resources/header.php"; ?>
 
 			<?php require "resources/main.php"; ?>
 
-			<?php require "resources/footer.php"; ?>1 -->	
+			<?php require "resources/footer.php"; ?>	
 		</div>	
 	</body>
 </html>
